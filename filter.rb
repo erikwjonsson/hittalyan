fb157b09fcd7c22d343f09afc2d303b34962cb31
The Benevolent Father <robinedman@gmail.com>
2012-08-03 11:37:27 +0200

    Changed integers to ranges in filter.

--- filter.rb
+++ filter.rb
@@ -1,7 +1,7 @@
 class Filter
   include Mongoid::Document
   belongs_to :user
-  field :rent, type: Integer
-  field :rooms, type: Integer
-  field :area, type: Integer  
+  field :rent, type: Range
+  field :rooms, type: Range
+  field :area, type: Range  
 end
