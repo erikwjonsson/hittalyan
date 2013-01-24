4bb61e0dce64a4fe4b42880f945b1981f4fdb5e2
EPMSOGonza <joke_a_87@hotmail.com>
2013-01-24 03:11:55 +0100

    default values for filter to scare away NilClass

--- models/filter.rb
+++ models/filter.rb
@@ -1,7 +1,7 @@
 class Filter
   include Mongoid::Document
   embedded_in :user
-  field :rooms, type: Range
-  field :rent, type: Integer
-  field :area, type: Range
+  field :rooms, type: Range, default: (0..0)
+  field :rent, type: Integer, default: 0
+  field :area, type: Range, default: (0..0)
 end
