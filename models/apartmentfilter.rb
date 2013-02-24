3bc1e0a7ba3a4d81ca38d06c1644c400c9bfe363
EPMSOGonza <joke_a_87@hotmail.com>
2013-02-24 15:12:56 +0100

    removed redundant filter model

--- models/apartmentfilter.rb
+++
@@ -1,9 +0,0 @@
-class ApartmentFilter
-  include Mongoid::Document
-  embedded_in :user
-  field :rent, type: Range
-  field :rooms, type: Range
-  field :area, type: Range
-	
-	validates :rent, :rooms, :area, presence: true
-end
