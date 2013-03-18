70a7a049ed352fd0e219ffcc1a3a836da484380f
robin <robin.edman@gmail.com>
2013-03-18 11:21:08 +0100

    When creating a user a filter that allows anything (instead of nothing) will be created by default.

--- models/filter.rb
+++ models/filter.rb
@@ -1,9 +1,9 @@
 class Filter
   include Mongoid::Document
   embedded_in :user
-  field :rooms, type: Range, default: (1..1)
-  field :rent, type: Integer, default: 1000
-  field :area, type: Range, default: (10..10)
+  field :rooms, type: Range, default: (1..999)
+  field :rent, type: Integer, default: 999999
+  field :area, type: Range, default: (10..9999)
 
   def to_hash
     hashified = {roomsMin: user.filter.rooms.first,
