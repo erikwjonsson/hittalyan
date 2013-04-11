066e9da34934ec8284d0cb18acf87ee22e712aea
EPMSOGonza <joke_a_87@hotmail.com>
2013-04-11 15:51:56 +0200

    now ignores directories when requiring files

--- helpers.rb
+++ helpers.rb
@@ -4,7 +4,9 @@
 
 # Require all files in a given subdirectory
 def require_from_directory(directory)
-  Dir[File.join(File.dirname(__FILE__), directory, '*')].each {|file| require file }
+  Dir[File.join(File.dirname(__FILE__), directory, '*')].each do |file|
+    require file unless File.directory?(file)
+  end
 end
 
 # Call this method like this:
