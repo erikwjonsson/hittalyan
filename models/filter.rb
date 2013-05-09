f14305f7d85558f047b7d4225ee011a07dd44f5f
robin <robin.edman@gmail.com>
2013-05-09 10:02:59 +0200

    City should be capitalized.

--- models/filter.rb
+++ models/filter.rb
@@ -4,5 +4,5 @@ class Filter
   field :rooms, type: Range, default: (1..999)
   field :rent, type: Integer, default: 999999
   field :area, type: Range, default: (10..9999)
-  field :cities, type: Array, default: ["stockholm"]
+  field :cities, type: Array, default: ["Stockholm"]
 end
