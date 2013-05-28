1ea43788c9c3a8c0a55dfcfcb91435f371f29131
EPMSOGonza <joke_a_87@hotmail.com>
2013-05-28 10:59:50 +0200

    Prettier and more commented

--- tools/populatecoupons.rb
+++ tools/populatecoupons.rb
@@ -4,25 +4,27 @@ require_relative '../init.rb'
 
 c = []
 
-# ==============================================
+# =============================================
 # Production coupons for production deployment
-# ==============================================
+# =============================================
 # c << Coupon.new(code: "1234test",
 #                 description: 
 #                 discount_in_percentage_units: 25,
 #                 valid: true)
 
-# ============================================
-# Coupon to use when there is no coupon
-# ============================================
+# ===========================================================
+# Coupon to use when there is no coupon.
+# This really needs to be here.
+# DO NOT REMOVE OR MODIFY unless you know what you're doing.
+# ===========================================================
 c << Coupon.new(code: "NONE",
                 description: "Not a valid code/coupon",
                 discount_in_percentage_units: 0,
                 valid: false)
 
-# ==========================================
+# =========================================
 # Test coupons for development and testing
-# ==========================================
+# =========================================
 unless production?
   c << Coupon.new(code: "VALID1234",
                   description: "This is a TESTcoupon for testing purposes. VALID",
