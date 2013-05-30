4ed407ddcf3dba0ca5d500184cd3d6762bcc5390
robin <robin.edman@gmail.com>
2013-05-30 11:36:19 +0200

    Respect permits_to_be_emailed.

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -1,6 +1,14 @@
 module Mailer
   OUR_STANDARD_EMAIL = "HittaLyan <lingonberryprod@gmail.com>"
   def self.shoot_email(recipient, subject, message, format='text', sender=OUR_STANDARD_EMAIL)
+    if recipient.is_a?(User)
+      unless recipient.permits_to_be_emailed
+        puts "Not sending email to #{recipient.email} because they don't want to be emailed."
+        return
+      end
+      recipient = recipient.email
+    end
+    
     email_params = {from: sender,
                     to: recipient,
                     subject: subject}
