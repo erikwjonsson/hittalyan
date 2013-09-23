68ee6082582f2fcfddcd87d69270ae7d4136efdf
robin <robin.edman@gmail.com>
2013-09-23 13:13:20 +0200

    Specified index for EmailHash.

--- models/emailhash.rb
+++ models/emailhash.rb
@@ -5,8 +5,10 @@ require 'digest'
 class EmailHash
   include Mongoid::Document
   include Mongoid::Timestamps
-                        
+
   field :hashed_email, type: String
+
+  index({hashed_email: 1 }, {unique: true})
 
   validates :hashed_email, presence: true
   validates :hashed_email, uniqueness: true
