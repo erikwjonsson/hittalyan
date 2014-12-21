bd8a6d9fc7ce0ec45387f0fcf9e827ff2b673b64
Robin Edman <robin.edman@gmail.com>
2014-12-21 17:03:57 +0000

    Updated tests config.

--- tests/user_test.rb
+++ tests/user_test.rb
@@ -1,5 +1,7 @@
 require 'minitest'
 require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
 require_relative '../init'
 
 class UserTest < Minitest::Test
