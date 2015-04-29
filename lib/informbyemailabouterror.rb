b0cffb7a24b5343012701d051d4ef8c9aabea31f
EPMSOGonza <joke_a_87@hotmail.com>
2015-04-29 13:30:16 +0200

    More informative mail

--- lib/informbyemailabouterror.rb
+++ lib/informbyemailabouterror.rb
@@ -1,14 +1,21 @@
 def inform_by_email_about_error(e)
   puts 'SENDING EMAIL ABOUT ERROR----------'
-  
+
   body = <<BODY
+#{'***This is a TEST***' unless production?}
 The following error was caught at #{Time.now}:
+Name:
+  #{e.class.name}
+Message:
+  #{e.message}
+Inspect:
+  #{e.inspect}
 Backtrace:
   #{e.backtrace}
 BODY
 
   Manmailer.shoot_email('hittalyanab@gmail.com',
-                        "Cubancabal Exception: #{e.class.name}",
+                        "#{'*Test* ' unless production?}Cubancabal Exception: #{e.class.name}",
                         body,
                         INFO_EMAIL,
                         INFO_NAME,
